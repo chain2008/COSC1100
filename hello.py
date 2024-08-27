446701bdfc26955b4b2f66dc8fa89c836d3125ea
Cheng He <chenghe.gm@gmail.com>
2024-08-27 11:24:33 -0400

    i

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 import util
 
 # This line is not part of the function
-print(util.cal_age('d'))
+if util.liquor_age('2000-07-01'):
+    print('Ok to sell liquor')
+else:
+    print('Cannot sell liquor')
 print(util.COLLEGE)
 i = 3
 j = 2 * -i
@@ -11,17 +14,3 @@ msg = "hello world"
 print(msg)
 
 
-from datetime import datetime
-# py -m pip install --upgrade pip
-# py -m pip install python-dateutil
-from dateutil.relativedelta import relativedelta
-
-# Define the two dates
-date1 = datetime(2017, 8, 14)
-date2 = datetime(2022, 3, 16)
-
-# Calculate the difference
-delta = relativedelta(date2, date1)
-
-# Print the difference in years
-print(f"Difference in years: {delta.years}")
--- util.py
+++ util.py
@@ -2,15 +2,17 @@
 COLLEGE = 'Durham College' # global constant
 semester = 'Fall 2024'  # global variable
 LIQUOR_AGE = 19
+DATE_FORMAT = '%Y-%m-%d'
 
-import datetime
+from datetime import datetime
+# py -m pip install --upgrade pip
+# py -m pip install python-dateutil
+from dateutil.relativedelta import relativedelta
 
-def cal_age(birthday): # [missing-function-docstring]
+def liquor_age(birthday): # [missing-function-docstring]
     """Function printing python version."""
-    # Function body
-    x = 1
-    y = 21
-    if y == 2:  # : defines a statement block, https://www.w3schools.com/python/python_syntax.asp
-        return x + y  # Still part of the function
-    else:
-        return x - y
+    birthday = datetime.strptime(birthday, DATE_FORMAT)
+    delta = relativedelta(datetime.now(), birthday)
+
+    return delta.years >= LIQUOR_AGE  # : defines a statement block, https://www.w3schools.com/python/python_syntax.asp
+
