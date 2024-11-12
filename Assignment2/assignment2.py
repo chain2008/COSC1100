3e28b669350d6f5ca03238c7cd2049dbd77f0395
Cheng He <chenghe.gm@gmail.com>
2024-11-12 15:20:23 -0500

    i

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -4,17 +4,23 @@ Author: Cheng He
 Date: 2024-11-1
 Version: 1
 """
+import sys
+import os
+# Add the parent directory to the system path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import util
 import base3cipher
 
-text = "This is a plain Text"
+text = util.get_input("Input plain Text: ")
 txt_length = len(text)
 key = base3cipher.gen_key(txt_length)
 cipher_text = ""
 for index in range(0, txt_length):
     cipher_text += base3cipher.base3_cipher(text[index],key[index])
-print(cipher_text)
+print(f"cipher: {cipher_text}")
 
 plain_text = ""
 for index in range(0, txt_length):
     plain_text += base3cipher.base3_cipher(cipher_text[index],key[index])
-print(plain_text)
+print(f"plain: {plain_text}")
