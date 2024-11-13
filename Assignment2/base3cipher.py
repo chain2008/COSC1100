8908da2b01ba32119a9c118696966ea7a0495918
Cheng He <chenghe.gm@gmail.com>
2024-11-13 08:45:07 -0500

    i

--- Assignment2/base3cipher.py
+++ Assignment2/base3cipher.py
@@ -6,7 +6,6 @@ ASCII_A = 64
 BASE = 3
 LENGTH = 3
 
-
 def base3_cipher(letter, key):
     # Convert the letter to its corresponding ASCII code.
     ascii_code = ord(letter.upper()) - ASCII_A
@@ -14,14 +13,7 @@ def base3_cipher(letter, key):
     if ascii_code < 0 or ascii_code >= 26:
         return letter
     # Convert the ASCII code to base 3 and store digits in a list.
-    digit_list = []
-    while ascii_code > 0:
-        # Get the remainder from dividing the code by 3.
-        remainder = ascii_code % BASE
-        # This remainder becomes the first digit of the numeric value.
-        digit_list.insert(0, remainder)
-        # Force integer division.
-        ascii_code = ascii_code // BASE
+    digit_list = decimal_base(ascii_code, BASE)
 
     # Add leading zeros (if necessary) to make it 3 digits long.
     while len(digit_list) < LENGTH:
@@ -33,10 +25,7 @@ def base3_cipher(letter, key):
     digit_list[key] = digit_list[0]
     digit_list[0] = hold_value
 
-    decimal_value = 0
-    # Now decode from ASCII which will result in an encoded letter.
-    decimal_value = digit_list[0] * \
-        (BASE * BASE) + digit_list[1] * (BASE) + digit_list[2]
+    decimal_value = base_decimal(BASE,digit_list)
 
     # Return the output as a single letter.
     ret = chr(decimal_value + ASCII_A)
@@ -48,3 +37,21 @@ def gen_key(length):
     for index in range(0, length):
         key[index] = random.randint(0, 2)
     return key
+
+def decimal_base(number,base):
+    digit_list = []
+    while number > 0:
+        # Get the remainder from dividing the code by 3.
+        remainder = number % base
+        # This remainder becomes the first digit of the numeric value.
+        digit_list.insert(0, remainder)
+        # Force integer division.
+        number = number // base
+    return digit_list
+
+def base_decimal(base, digit_list):
+    decimal_value = 0
+    # Now decode from ASCII which will result in an encoded letter.
+    decimal_value = digit_list[0] * \
+        (base * base) + digit_list[1] * (base) + digit_list[2]
+    return decimal_value
