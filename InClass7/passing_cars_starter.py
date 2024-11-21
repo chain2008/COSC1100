3f160cea5b560b8835174b5b0aa4014411b17625
Cheng He <chenghe.gm@gmail.com>
2024-11-21 12:50:15 -0500

    i

--- InClass7/passing_cars_starter.py
+++ InClass7/passing_cars_starter.py
@@ -6,7 +6,10 @@
 
 import tkinter as tk
 from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText
 from idlelib.tooltip import Hovertip
+
+import util
 
 MINUTES_PER_HOUR = 60
 WINDOW_WIDTH = 300
@@ -21,12 +24,6 @@ window.geometry(f"{WINDOW_WIDTH}x{WINDOW_HEIGHT}")
 window.minsize(width = WINDOW_MIN_WIDTH, height = WINDOW_MIN_HEIGHT)
 
 window.title("Should I Pass?")
-window.grid_columnconfigure(0, weight=1)
-window.grid_columnconfigure(1, weight=2)
-window.grid_rowconfigure(0, weight=1)
-window.grid_rowconfigure(1, weight=1)
-window.grid_rowconfigure(2, weight=3)
-window.grid_rowconfigure(3, weight=1)
 
 label_1 = ttk.Label(window, text="Your Speed")
 label_2 = ttk.Label(window, text="Target Speed")
@@ -35,7 +32,7 @@ speed_2 = tk.DoubleVar(value = 0)
 output = tk.StringVar()
 entry_speed_1 = ttk.Entry(window, textvariable=speed_1, width=30) #30 characters
 entry_speed_2 = ttk.Entry(window, textvariable=speed_2, width=30) #30 characters
-label_output = ttk.Label(window, textvariable=output)
+label_output = ScrolledText(window, height=5)
 
 def calculate(event= None):
     try:
@@ -55,6 +52,8 @@ def calculate(event= None):
             "km/h to " + str(round(high_speed, 1)) + \
             "km/h can gain you " + str(round(speed_difference_seconds, 2)) + \
             " kilometres per minute.")
+        label_output.delete('1.0', tk.END)
+        label_output.insert(tk.END,output.get())
 
     # If they don’t both have numbers:
     except Exception as exp:
@@ -66,16 +65,6 @@ def reset():
     speed_2.set(0)
     output.set("")
     entry_speed_1.focus()
-
-# Now you need to set up the widgets!
-# Note that if you use the exact names I did in the functions above, you won't need to modify those.
-# If you use a different naming scheme, expect the functions to require some changes.
-
-
-# Row 0 widgets.
-# Add widgets for all rows, based on your plan.
-# Add hotkey support.
-# Anything else?
 
 btn_calculation = ttk.Button(
    window,
@@ -89,16 +78,10 @@ btn_reset = ttk.Button(
    compound=tk.LEFT,
    command=reset
 )
+util.layout(window, label_1=label_1, label_2=label_2, \
+            entry_speed_1=entry_speed_1,entry_speed_2=entry_speed_2, \
+            label_output=label_output,
+            btn_calculation=btn_calculation,btn_reset=btn_reset)
 
-label_1.grid(row=0, column=0, sticky="nsew")
-label_2.grid(row=1, column=0, sticky="nsew")
-
-entry_speed_1.grid(row=0, column=1, sticky="nsew")
-entry_speed_2.grid(row=1, column=1, sticky="nsew")
-
-label_output.grid(row=2, column=0, columnspan=2, sticky="nsew")
-
-btn_calculation.grid(row=3, column=1, sticky="w")
-btn_reset.grid(row=3, column=1, sticky="e")
 
 window.mainloop()
--- InClass7/util.py
+++ InClass7/util.py
@@ -1,1 +1,18 @@
 def layout(master, **widgets):
+    master.grid_columnconfigure(0, weight=1)
+    master.grid_columnconfigure(1, weight=2)
+    master.grid_rowconfigure(0, weight=1)
+    master.grid_rowconfigure(1, weight=1)
+    master.grid_rowconfigure(2, weight=3)
+    master.grid_rowconfigure(3, weight=1)
+
+    widgets["label_1"].grid(row=0, column=0, sticky="nsew")
+    widgets["label_2"].grid(row=1, column=0, sticky="nsew")
+
+    widgets["entry_speed_1"].grid(row=0, column=1, sticky="nsew")
+    widgets["entry_speed_2"].grid(row=1, column=1, sticky="nsew")
+
+    widgets["label_output"].grid(row=2, column=0, columnspan=2, sticky="nsew")
+
+    widgets["btn_calculation"].grid(row=3, column=1, sticky="w")
+    widgets["btn_reset"].grid(row=3, column=1, sticky="e")
